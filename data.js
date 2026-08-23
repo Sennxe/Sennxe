0a5a8987b24512dc58ab992dcf13cccd02bc89cb
Senn <sennxe@gmail.com>
2026-08-24 07:37:25 +0900

    UpDate: 자바 스크립트 수정

--- data.js
+++ data.js
@@ -8,17 +8,18 @@ const motorSportsData = {
     }
 };
 const portfolioData = {
-    "01_Dahlia": {
-        "title": "Dahlia",
+    "01_Junko": {
+        "title": "Junko",
         "images": [
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon0.png",
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon1.png",
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon2.png",
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon3.png",
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon4.png",
-            "Portfolio/01_Dahlia/Dahlia_KimChae-yeon5.png"
+            "Portfolio/01_Junko/Junko_KimChae-yeon0.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon00.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon1.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon2.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon3.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon4.png",
+            "Portfolio/01_Junko/Junko_KimChae-yeon5.png"
         ],
-        "youtube": "https://www.youtube.com/watch?v=wAIwBMrVG5Y&list=PLcl_lgdUTOv4&index=2"
+        "youtube": "https://www.youtube.com/watch?v=lZg_M6k3op8&list=PLcl_lgdUTOv4&index=3"
     },
     "02_Sorai Saki": {
         "title": "Sorai Saki",
@@ -32,30 +33,56 @@ const portfolioData = {
         ],
         "youtube": ""
     },
-    "03_Wriothesley": {
+    "03_Dahlia": {
+        "title": "Dahlia",
+        "images": [
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon0.png",
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon1.png",
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon2.png",
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon3.png",
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon4.png",
+            "Portfolio/03_Dahlia/Dahlia_KimChae-yeon5.png"
+        ],
+        "youtube": "https://www.youtube.com/watch?v=wAIwBMrVG5Y&list=PLcl_lgdUTOv4&index=2"
+    },
+    "04_Wriothesley": {
         "title": "Wriothesley",
         "images": [
-            "Portfolio/03_Wriothesley/Wriothesley_KimChae-yeon0.png",
-            "Portfolio/03_Wriothesley/Wriothesley_KimChae-yeon1.png",
-            "Portfolio/03_Wriothesley/Wriothesley_KimChae-yeon2.png",
-            "Portfolio/03_Wriothesley/Wriothesley_KimChae-yeon4.png"
+            "Portfolio/04_Wriothesley/Wriothesley_KimChae-yeon0.png",
+            "Portfolio/04_Wriothesley/Wriothesley_KimChae-yeon1.png",
+            "Portfolio/04_Wriothesley/Wriothesley_KimChae-yeon2.png",
+            "Portfolio/04_Wriothesley/Wriothesley_KimChae-yeon4.png"
         ],
         "youtube": ""
     },
-    "04_Post Dochi (Vtuber)": {
+    "05_Post Dochi (Vtuber)": {
         "title": "Post Dochi (Vtuber)",
         "images": [
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (1).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (10).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (2).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (3).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (4).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (5).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (6).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (7).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (8).png",
-            "Portfolio/04_Post Dochi (Vtuber)/Vtuber_KimChae-yeon (9).png"
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon0.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon1.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon2.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon3.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon4.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon5.png",
+            "Portfolio/05_Post Dochi (Vtuber)/Dochi_KimChae-yeon6.png",
+            "Portfolio/05_Post Dochi (Vtuber)/dochi0_KimChae-Yeon.png"
         ],
         "youtube": "https://www.youtube.com/watch?v=Xu7oVEqaouE&list=PLcl_lgdUTOv4"
+    },
+    "06_AI+Unity": {
+        "title": "AI+Unity",
+        "images": [
+            "Portfolio/06_AI+Unity/AIUnity0.png",
+            "Portfolio/06_AI+Unity/AIUnity1.png"
+        ],
+        "youtube": ""
+    },
+    "07_AI+GitHub": {
+        "title": "AI+GitHub",
+        "images": [
+            "Portfolio/07_AI+GitHub/AIWebsite0.png",
+            "Portfolio/07_AI+GitHub/AIWebsite1.png"
+        ],
+        "youtube": ""
     }
 };
